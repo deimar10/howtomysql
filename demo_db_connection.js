15c619bb3898ccfa08c0a9439d88e5dadc3aaa5b
deimar <deimar.parn@khk.ee>
2022-02-02 20:10:44 +0000

    Add values to table and use SELECT * FROM and WHERE in my table

--- demo_db_connection.js
+++ demo_db_connection.js
@@ -11,10 +11,8 @@ var con = mysql.createConnection({
 
 con.connect(function(err) {
     if (err) throw err;
-    console.log("Connected!");
-    var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
-    con.query(sql, function (err, result) {
+    con.query("SELECT * FROM customers WHERE address = 'Park Lane 38'", function (err, result) {
         if (err) throw err;
-        console.log("1 record inserted");
+        console.log(result);
     });
 });
