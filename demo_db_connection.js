d5e5eff2b863a58d71383ed124516b8f946e647d
deimar <deimar.parn@khk.ee>
2022-03-23 11:10:52 +0000

    Node.js MySQL Order By

--- demo_db_connection.js
+++ demo_db_connection.js
@@ -11,7 +11,7 @@ var con = mysql.createConnection({
 
 con.connect(function(err) {
     if (err) throw err;
-    con.query("SELECT * FROM customers", function (err, result, fields) {
+    con.query("SELECT * FROM customers ORDER BY name", function (err, result) {
         if (err) throw err;
         console.log(result);
     });
