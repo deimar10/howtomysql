d93503565ea210d656f582c11e15653d3c73eb1a
deimar <deimar.parn@khk.ee>
2022-03-23 11:09:19 +0000

    Node.js MySQL Select From

--- demo_db_connection.js
+++ demo_db_connection.js
@@ -11,7 +11,7 @@ var con = mysql.createConnection({
 
 con.connect(function(err) {
     if (err) throw err;
-    con.query("SELECT * FROM customers WHERE address = 'Park Lane 38'", function (err, result) {
+    con.query("SELECT * FROM customers", function (err, result, fields) {
         if (err) throw err;
         console.log(result);
     });
