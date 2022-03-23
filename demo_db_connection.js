83b41a2a1235bac568094b48811ac57086a21401
deimar <deimar.parn@khk.ee>
2022-03-23 11:13:23 +0000

    Node.js MySQL Update

--- demo_db_connection.js
+++ demo_db_connection.js
@@ -11,7 +11,8 @@ var con = mysql.createConnection({
 
 con.connect(function(err) {
     if (err) throw err;
-    con.query("SELECT * FROM customers ORDER BY name", function (err, result) {
+    var sql = "UPDATE customers SET address = 'Canyon 123' WHERE address = 'Valley 345'";
+    con.query(sql, function (err, result) {
         if (err) throw err;
         console.log(result);
     });
